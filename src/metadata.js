1c9be6062f407f7338c50b1639ca63a2810f88db
dreptin <clement.dreptin@inist.fr>
2022-06-10 15:23:10 +0200

    fix(hal-typologies): added missing halTypologies entry in metadata.js

--- src/metadata.js
+++ src/metadata.js
@@ -1,4 +1,5 @@
 module.exports = {
   mappings: require('./metadata/metadata-mappings.json'),
   metadataXpaths: require('./metadata/metadata-xpaths.json'),
+  halTypologies: require('./metadata/hal-typology-mappings.json'),
 };
